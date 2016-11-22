e27c7eb077e00fc54e217beb72fe90c372fbe304
Ryan Turner <ryan.e.t@gmail.com>
2016-11-21 21:44:21 -0600

    Prometheus role now does everything besides the templates for grafana. Yay.

---
+++ roles/prometheus/templates/dynamic_inventory.py
@@ -0,0 +1,12 @@
+import sys,json,urllib2
+import ssl
+
+# This restores the same behavior as before.
+context = ssl._create_unverified_context()
+data = urllib2.urlopen("https://portal.memhamwan.net/host/ansible.json", context=context)
+jsonData = json.load(data)
+finalData = [{'targets': [], 'labels': {}}]
+for server in jsonData[sys.argv[1]]:
+    finalData[0]['targets'].append(server)
+with open(sys.argv[2], "w") as text_file:
+    text_file.write(json.dumps(finalData, sort_keys=True, indent=2))
--- roles/prometheus/templates/dynamic_inventory.sh
+++
@@ -1,8 +0,0 @@
-import sys,json,urllib2
-
-data = urllib2.urlopen("https://portal.memhamwan.net/host/ansible.json")
-jsonData = json.load(data)
-finalData = [{'targets': [], 'labels': {}}]
-for server in jsonData['mikrotik']:
-    finalData[0]['targets'].append(server)
-print json.dumps(finalData, sort_keys=True, indent=2)
